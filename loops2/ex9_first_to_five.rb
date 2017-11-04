87ce7f061493aeb67e9006ba6a6775bcb0521967
Travis Walker <traviswalkerdev@gmail.com>
2017-11-04 16:46:14 -0700

    Updated solution for Loops part 2 ex9

--- loops2/ex9_first_to_five.rb
+++ loops2/ex9_first_to_five.rb
@@ -14,3 +14,28 @@ loop do
   puts "5 was reached!"
   break
 end
+
+
+
+# Is using next the best option for this example?
+# Could you use an if/else statement instead?
+
+number_a = 0
+number_b = 0
+
+loop do
+  number_a += rand(2)
+  number_b += rand(2)
+
+  # puts number_a
+  # puts number_b
+  # puts
+
+  if number_a == 5 || number_b == 5
+    puts "5 was reached!"
+    break
+  end
+
+end
+
+# To me, the if version reads better.
