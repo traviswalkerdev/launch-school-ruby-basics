90b0da32d8a1f8700f6a73169ed125ccb6392a84
Travis Walker <traviswalkerdev@gmail.com>
2017-11-04 16:26:36 -0700

    Loops part 2 ex7 solution

---
+++ loops2/ex7_stop_counting.rb
@@ -0,0 +1,29 @@
+5.times do |index|
+  puts index
+  break if index == 2
+end
+
+
+
+
+# how many values will be printed?
+
+# 5.times do |index|
+#   puts index
+#   break if index == 4
+# end
+
+# prints 5 values, 0 through 4
+
+
+
+
+# how many values will be printed?
+
+# 5.times do |index|
+#   puts index
+#   break if index < 7
+# end
+
+# prints only 0 because 0 is less than 7 and the loop stops
+# after the first iteration
