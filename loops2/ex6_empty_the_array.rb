9eb8b5eeb2e48058eca8e9d5372c42783f3a0cde
Travis Walker <traviswalkerdev@gmail.com>
2017-11-04 16:29:03 -0700

    updated Loops part 2 ex6 solution to include FUrther Exploration solution

--- loops2/ex6_empty_the_array.rb
+++ loops2/ex6_empty_the_array.rb
@@ -4,3 +4,15 @@ loop do
   puts names.shift
   break if names.size == 0
 end
+
+
+# Our solution prints the names from first (Sally) to last (Henry).
+# Can you change this to print the names from last to first?
+puts
+
+names = ['Sally', 'Joe', 'Lisa', 'Henry']
+
+loop do
+  puts names.pop
+  break if names.size == 0
+end
