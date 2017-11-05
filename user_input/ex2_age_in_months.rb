ee40c6e2c8bda12833bb5453b45469b90dcc2506
Travis Walker <traviswalkerdev@gmail.com>
2017-11-04 17:20:56 -0700

    Updated User Input ex2 to include Further Exploration solution

--- user_input/ex2_age_in_months.rb
+++ user_input/ex2_age_in_months.rb
@@ -1,3 +1,9 @@
 puts ">> What is your age in years?"
 age = gets.chomp.to_i
 puts "You are #{age * 12} months old."
+
+
+
+# What happens if you enter a non-numeric value for the age?
+
+# Every non-numeric value returns 0 months.
