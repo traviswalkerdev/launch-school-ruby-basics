96f9aa012c1d0f9e65a4555b8bd58b0daa4ec021
Travis Walker <traviswalkerdev@gmail.com>
2017-11-07 18:48:43 -0800

    Methods ex5 updated with Further Exploration solutions

--- methods/ex5_make_and_model.rb
+++ methods/ex5_make_and_model.rb
@@ -3,3 +3,18 @@ def car(make, model)
 end
 
 car("Toyota", "Corolla")
+
+
+
+##################
+
+# Further Exploration
+
+# def car(make, model)
+#   "#{make} #{model}"
+# end
+
+# puts car("Toyota", "Corolla")
+
+# Original method returns nil because the last line is puts
+# Revised method returns the actual string
