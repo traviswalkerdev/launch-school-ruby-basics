a2be0f05670db42f49971a16698afb1397cc919f
Travis Walker <traviswalkerdev@gmail.com>
2017-11-07 18:25:17 -0800

    User Input ex10 refactored number validation into method to remove duplication

--- user_input/ex10_opposites_attract.rb
+++ user_input/ex10_opposites_attract.rb
@@ -2,36 +2,26 @@ def valid_number?(number_string)
   number_string.to_i.to_s == number_string && number_string.to_i != 0
 end
 
+def read_number
+  loop do
+    puts ">> Please enter a positive or negative integer:"
+    number = gets.chomp
+
+    return number.to_i if valid_number?(number)
+    puts ">> Invalid input. Only non-zero integers are allowed."
+  end
+end
+
 first_number = nil
 second_number = nil
 
 loop do
-  loop do
-    puts ">> 1) Please enter a positive or negative integer:"
-    first_number = gets.chomp
-
-    if first_number.to_i == 0
-      puts ">> Invalid input. Only non-zero integers are allowed."
-    elsif valid_number?(first_number)
-      break
-    end
-  end
-
-  loop do
-    puts ">> 2) Please enter a positive or negative integer:"
-    second_number = gets.chomp
-
-    if second_number.to_i == 0
-      puts ">> Invalid input. Only non-zero integers are allowed."
-    elsif valid_number?(second_number)
-      break
-    end
-  end
+  first_number = read_number
+  second_number = read_number
 
   break if first_number.to_i * second_number.to_i < 0
   puts ">> Sorry. One integer must be positive, one must be negative."
   puts ">> Please start over."
-
 end
 
 result = first_number.to_i + second_number.to_i
