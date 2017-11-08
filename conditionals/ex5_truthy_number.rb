2c5a214a1fc0b24ee9f8c42e9690a269ad2477d0
Travis Walker <traviswalkerdev@gmail.com>
2017-11-07 20:42:32 -0800

    Conditionals ex5 solution

---
+++ conditionals/ex5_truthy_number.rb
@@ -0,0 +1,10 @@
+number = 7
+
+if number
+  puts "My favorite number is #{number}."
+else
+  puts "I don't have a favorite number."
+end
+
+
+# prints the first string because only false and nil are false
