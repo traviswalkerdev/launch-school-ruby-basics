85fdd5af9b64224bb6947858d51117c74d955669
Travis Walker <traviswalkerdev@gmail.com>
2017-11-08 17:34:38 -0800

    Strings ex10 solution

---
+++ strings/ex10_are_you_there.rb
@@ -0,0 +1,15 @@
+colors = "blue pink yellow orange"
+
+puts colors.split.include?("yellow")
+puts colors.split.include?("purple")
+
+
+
+##################
+# Further Exploration
+
+
+# colors = "blue boredom yellow"
+
+# colors.include?("red") => true because in the string
+# colors.split.include?("red") => false because we split into an array first
